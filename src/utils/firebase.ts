41a4646e9129e5d8740b9afec48da4b7b4f3d36f
Trisna Utama <trisnautama@gmail.com>
2025-03-29 20:35:33 +0700

    feat: initialize app

---
+++ src/utils/firebase.ts
@@ -0,0 +1,26 @@
+import { initializeApp, getApps, getApp } from "firebase/app";
+import { getAnalytics, isSupported } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_MEASUREMENT_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApp();
+const db = getFirestore(app);
+const storage = getStorage(app);
+const analytics =
+  typeof window !== "undefined"
+    ? await isSupported().then((supported) =>
+        supported ? getAnalytics(app) : null
+      )
+    : null;
+
+export { app, db, storage, analytics };
