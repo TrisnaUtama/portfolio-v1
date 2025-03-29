628e4b7d1d850e9c916ee878a3ab07181a522466
Trisna Utama <trisnautama@gmail.com>
2025-03-29 20:40:55 +0700

    adding configuration for an image

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ["storage.trisnautama.site"],
+  },
+  experimental: {
+    serverActions: {
+      bodySizeLimit: "20mb",
+    },
+  },
+  reactStrictMode: false,
 };
 
 export default nextConfig;
